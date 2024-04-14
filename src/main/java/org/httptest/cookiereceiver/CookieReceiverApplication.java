32b362261c167a977cb9e3a37f2e98e9dea20449
leejongwook2 <34103253+leejongwook2@users.noreply.github.com>
2024-04-14 20:18:40 +0900

    Initial commit

---
+++ src/main/java/org/httptest/cookiereceiver/CookieReceiverApplication.java
@@ -0,0 +1,13 @@
+package org.httptest.cookiereceiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookieReceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookieReceiverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/httptest/cookiereceiver/CookieReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.httptest.cookiereceiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookieReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
